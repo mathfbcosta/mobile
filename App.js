c36b21f8c4638cb2d2c09b487ddb86ca2a94b48a
Matheus <matheusfbcosta@gmail.com>
2023-04-29 19:16:29 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+const [phoneNumber, setPhoneNumber] = useState('');
+const formatPhoneNumber = (text) => {
+  let formattedText = text;
+
+  if (text.length === 3) {
+    formattedText = `(${text}) `;
+  } else if (text.length === 9) {
+    formattedText = `${text}-`;
+  } else if (text.length > 14) {
+    formattedText = text.substring(0, 14);
+  }
+
+  setPhoneNumber(formattedText);
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Insira seu número de telefone!</Text>
+      <TextInput
+        keyboardType="phone-pad"
+        value={phoneNumber}
+        onChangeText={formatPhoneNumber}
+        type={Number}
+        prop maxLength={11}
+        style={
+          {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#fff',
+            borderWidth: 0.5,
+            borderColor: '#000',
+            borderRadius: 5,
+            padding: 10,
+            margin: 10,
+            height: 40,
+            width: 140,
+            resizeMode: 'stretch',
+            alignItems: 'center'
+          }
+        }
+        placeholder="(XXX) XXXXX-XXXX"
+        underlineColorAndroid="transparent"
+      />
+
+      <StatusBar style="dark" />
     </View>
+
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00395c',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
