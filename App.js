a2874ff26eaa202a18a68cc489d4e0d08b2711ef
Matheus <matheusfbcosta@gmail.com>
2023-04-29 20:51:43 -0400

    Adicionado o TextImputMask para formatar o telefone.

--- App.js
+++ App.js
@@ -1,66 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-
-const [phoneNumber, setPhoneNumber] = useState('');
-const formatPhoneNumber = (text) => {
-  let formattedText = text;
-
-  if (text.length === 3) {
-    formattedText = `(${text}) `;
-  } else if (text.length === 9) {
-    formattedText = `${text}-`;
-  } else if (text.length > 14) {
-    formattedText = text.substring(0, 14);
-  }
-
-  setPhoneNumber(formattedText);
-};
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { TextInputMask } from 'react-native-masked-text';
 
 export default function App() {
+  const [cell, setCell] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Insira seu número de telefone!</Text>
-      <TextInput
+      <Text style={styles.text} >Insira seu número de telefone!</Text>
+      <TextInputMask style={styles.celNumber}
+        type={'cel-phone'}
+        options={{
+          maskType: 'BRL',
+          withDDD: true,
+          dddMask: '(99)'
+        }}
+        value={cell}
+        onChangeText={text => setCell(text)}
+        prop maxLength={14}
         keyboardType="phone-pad"
-        value={phoneNumber}
-        onChangeText={formatPhoneNumber}
-        type={Number}
-        prop maxLength={11}
-        style={
-          {
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#fff',
-            borderWidth: 0.5,
-            borderColor: '#000',
-            borderRadius: 5,
-            padding: 10,
-            margin: 10,
-            height: 40,
-            width: 140,
-            resizeMode: 'stretch',
-            alignItems: 'center'
-          }
-        }
-        placeholder="(XXX) XXXXX-XXXX"
+        placeholder="(XX) XXXXX-XXXX"
         underlineColorAndroid="transparent"
+
+
       />
-
       <StatusBar style="dark" />
-    </View>
-
+    </View >
   );
 }
 
-
 const styles = StyleSheet.create({
+  text: {
+    color: '#fff'
+  },
+  celNumber: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+    height: 40,
+    width: 140,
+    resizeMode: 'stretch',
+    alignItems: 'center'
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#00395c',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
-
